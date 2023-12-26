eb87d8cd1cecc01e4a94dc86486dfa3d7bf24bc8
Taofeek Abdulazeez <102738186+Taofeekabdulazeez@users.noreply.github.com>
2023-12-26 09:40:49 +0100

    Update Overlay.jsx

--- src/components/Overlay.jsx
+++ src/components/Overlay.jsx
@@ -1,7 +1,7 @@
 function Overlay({ isOpen }) {
   if (isOpen)
     return (
-      <div className="fixed top-[60px] left-0 right-0 bottom-0 bg-[rgba(0,0,0,0.3)] backdrop-blur-[1px]"></div>
+      <div className="transition-all duration-300 fixed top-[60px] left-0 right-0 bottom-0 bg-[rgba(0,0,0,0.3)] backdrop-blur-[1px]"></div>
     );
 }
 
